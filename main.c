8f9116a831739087311567267d2d89c7c3f13c83
Mykola <nchernov@gmail.com>
2024-05-23 13:14:22 +0300

    feel operators

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
-void main()
+int main()
 {
-    printf("Hello, World!\n");
+    unsigned char a = -10;
+    int b = 100000;
+    double c = -30.14;
+
+    b = a = 5;
+
+    b = a + 5 + a;
+
+    a = -b;
+    a = +b + 7;
+
+    a = b ? 1 : 0;
+
+    a = !1;
+    b = ++a;
+    a = (char)c;
+    a = 1;
+    b = 1 << 20;
+
+    int result = a + 10;
+    printf("Result: %d len=%lu\n", result, sizeof(double));
+    return 0;
 }
